af047c5baa5a98ad3b0e15734a8b86a70f594d12
Pablo Bollansée <pablo.bollansee@gmail.com>
2013-01-22 23:43:57 +0100

    Added loading of database from the internet. The loading/downloading of the database is now done in a couritine so it doesn't stop the game. Images are loaded based on their name, they are very fast, other information comes later. The cards show their power/touchness bigger so it's easy to see.

--- Unity/Assets/Resources/Card Info/CardInfoManager.cs
+++ Unity/Assets/Resources/Card Info/CardInfoManager.cs
@@ -1,33 +1,15 @@
 using UnityEngine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Xml;
 using System.IO;
-
-public class CardInfo {
-	public string Name {get; set;}
-	public string Cost {get; set;}
-	public string Power {get; set;}
-	public string Toughness {get; set;}
-	public string Rules {get; set;}
-	public string Flavor {get; set;}
-	
-	private Material _imageMaterial;
-	public Material ImageMaterial {
-		get {
-			if (_imageMaterial == null) {
-				var manager = CardInfoManager.Instance;
-				_imageMaterial = new Material(manager.CardShader);
-				_imageMaterial.mainTexture = manager.CardBack;
-				manager.StartCoroutine(manager.LoadCardImageInto(Name, _imageMaterial));
-			}
-			return _imageMaterial;
-		}
-	}
-}
+using System.Threading;
 
 public class CardInfoManager : MonoBehaviour {
+	public const int NUMBER_OF_CARDS_TO_READ_PER_FRAME = 100;
+	
 	static CardInfoManager _instance;
 	public static CardInfoManager Instance { get { return _instance; } }
 	
@@ -49,93 +31,232 @@ public class CardInfoManager : MonoBehaviour {
 	public TextAsset _database;
 	
 	// Info
-	Dictionary<string, CardInfo> _cardInfo = new Dictionary<string, CardInfo>();
-	public Dictionary<string, CardInfo> CardInfo {
-		get {
-			return _cardInfo;
-		}
-	}
+	private Dictionary<string, CardInfo> _cardInfo = new Dictionary<string, CardInfo>();
+	
 	
 	void Awake() {
 		if (_instance != null) throw new UnityException("Creating more than one CardImageManager");
 		_instance = this;
+		
+		StartCoroutine(LoadDatabase());
 	}
 	
-	void Start() {
+	public CardInfo GetCardInfo(string name) {
+		if (!_cardInfo.ContainsKey(name)) {
+			_cardInfo.Add(name, new CardInfo() {
+				Name = name
+			});
+		}
+		return _cardInfo[name];
+	}
+	
+	public IEnumerator LoadDatabase() {
 		float startTime = Time.realtimeSinceStartup;
-		LoadDatabase();
-		Debug.Log("Loaded Database in " + (Time.realtimeSinceStartup - startTime) + " seconds.");
-	}
-	
-	
-	public void LoadDatabase() {
-		MemoryStream assetStream = new MemoryStream(_database.bytes);
 		
-		XmlReaderSettings settings = new XmlReaderSettings();
-		settings.IgnoreComments = true;
-		settings.IgnoreWhitespace = true;
-		XmlReader reader = XmlReader.Create(assetStream, settings);
+		var www = new WWW("https://dl.dropbox.com/u/10448192/Magic%20Player/CardDatabase/mtg_card_database.xml");
+		yield return www;
+		if (www.error != null) {
+			Debug.Log("Could not load databse: " + www.error);
+		}
+		else {
+			Debug.Log("Downloaded Database in " + (Time.realtimeSinceStartup - startTime) + " seconds.");
 			
-		CardInfo tempCardInfo = new CardInfo();
-		
-		while (reader.Read())
-		{
-			// Only detect start elements.
-			if (reader.IsStartElement())
+			MemoryStream assetStream = new MemoryStream(www.bytes);
+			
+			XmlReaderSettings settings = new XmlReaderSettings();
+			settings.IgnoreComments = true;
+			settings.IgnoreWhitespace = false;
+			XmlReader reader = XmlReader.Create(assetStream, settings);
+				
+			CardInfo tempCardInfo = new CardInfo();
+			
+			int cardsParsedThisFrame = 0;
+			while (reader.Read())
 			{
-				// Get element name and switch on it.
-				switch (reader.Name)
+				// Only detect start elements.
+				if (reader.IsStartElement())
 				{
+					// Get element name and switch on it.
+					switch (reader.Name)
+					{
 					case "card":
-					tempCardInfo = new CardInfo();
-					break;
-					
+						tempCardInfo = new CardInfo();
+						break;
+						
+					case "id":
+						tempCardInfo.IDs.Add(reader.ReadElementContentAsString());
+						break;
+						
 					case "name":
-					tempCardInfo.Name = reader.ReadElementContentAsString();
-					break;
-					
+						tempCardInfo.Name = reader.ReadElementContentAsString();
+						break;
+						
 					case "cost":
-					tempCardInfo.Cost = reader.ReadElementContentAsString();
-					break;
-					
+						tempCardInfo.Cost = reader.ReadElementContentAsString();
+						break;
+						
 					case "power":
-					tempCardInfo.Power = reader.ReadElementContentAsString();
-					break;
-					
+						tempCardInfo.Power = reader.ReadElementContentAsString();
+						break;
+						
 					case "toughness":
-					tempCardInfo.Toughness = reader.ReadElementContentAsString();
-					break;
-					
+						tempCardInfo.Toughness = reader.ReadElementContentAsString();
+						break;
+						
 					case "rules":
-					tempCardInfo.Rules = reader.ReadElementContentAsString();
-					break;
-					
+						tempCardInfo.Rules = reader.ReadElementContentAsString();
+						break;
+						
 					case "flavor":
-					tempCardInfo.Flavor = reader.ReadElementContentAsString();
-					break;
+						tempCardInfo.Flavor = reader.ReadElementContentAsString();
+						break;
+					}
+				}
+				else {
+					if (reader.Name == "card") {
+						if (_cardInfo.ContainsKey(tempCardInfo.Name)) {
+							_cardInfo[tempCardInfo.Name].AddInfo(tempCardInfo);
+						}
+						else {
+							_cardInfo.Add (tempCardInfo.Name, tempCardInfo);
+						}
+						tempCardInfo = null;
+						
+						// Don't read all in the same frame to prevent stuttering.
+						++cardsParsedThisFrame;
+						if (cardsParsedThisFrame >= NUMBER_OF_CARDS_TO_READ_PER_FRAME) {
+							cardsParsedThisFrame = 0;
+							yield return null;
+						}
+					}
 				}
 			}
-			else {
-				if (reader.Name == "card") {
-					if (!_cardInfo.ContainsKey(tempCardInfo.Name)) _cardInfo.Add (tempCardInfo.Name, tempCardInfo);
-					tempCardInfo = null;
-				}
-			}
+		
+			Debug.Log("Loaded Database in " + (Time.realtimeSinceStartup - startTime) + " seconds.");
 		}
 	}
 	
-	public IEnumerator LoadCardImageInto(string name, Material material) {
-		var www = new WWW(CardInfoManager.CardImageURLByName(name));
-		while (!www.isDone) {
-			yield return new WaitForSeconds(.5f);
+	public IEnumerator LoadCardImageInto(CardInfo info, Material material) {
+		var www = new WWW(info.ImageURL);
+		yield return www;
+		
+		if (www.error == null) {
+			material.mainTexture = new Texture2D(4, 4, TextureFormat.DXT1, false);
+			www.LoadImageIntoTexture(material.mainTexture as Texture2D);
 		}
-		material.mainTexture = new Texture2D(4, 4, TextureFormat.DXT1, false);
-		www.LoadImageIntoTexture(material.mainTexture as Texture2D);
-	}
-	public static string CardImageURLByName(string name)
-	{
-		string nameFixed = name.Replace(" ", "%20");
-		//return "http://gatherer.wizards.com/Handlers/Image.ashx?name=" + nameFixed + "&type=card";
-		return "http://deckbox.org/mtg/" + nameFixed + "/tooltip.jpg";
+		else {
+			Debug.Log("Card not found: " + www.error);
+		}
 	}
 }
+
+public class CardInfo {
+	public const string UNUSED_VALUE = "";
+	
+	public event SimpleEventHandler Updated;
+	
+	private List<string> _ids = new List<string>();
+	public List<string> IDs {
+		get {
+			return _ids;
+		}
+		set {
+			_ids = value;
+		}
+	}
+	
+	private string _name = "No Name In Database";
+	public string Name {
+		get {
+			return _name;
+		}
+		set {
+			_name = value;
+		}
+	}
+	
+	private string _cost = UNUSED_VALUE;
+	public string Cost {
+		get {
+			return _cost;
+		}
+		set {
+			_cost = value;
+		}
+	}
+	
+	private string _power = UNUSED_VALUE;
+	public string Power {
+		get {
+			return _power;
+		}
+		set {
+			_power = value;
+		}
+	}
+	
+	private string _toughness = UNUSED_VALUE;
+	public string Toughness {
+		get {
+			return _toughness;
+		}
+		set {
+			_toughness = value;
+		}
+	}
+	
+	private string _rules = UNUSED_VALUE;
+	public string Rules {
+		get {
+			return _rules;
+		}
+		set {
+			_rules = value;
+		}
+	}
+	
+	private string _flavor = UNUSED_VALUE;
+	public string Flavor {
+		get {
+			return _flavor;
+		}
+		set {
+			_flavor = value;
+		}
+	}
+	
+	
+	private Material _imageMaterial;
+	public Material ImageMaterial {
+		get {
+			if (_imageMaterial == null) {
+				var manager = CardInfoManager.Instance;
+				_imageMaterial = new Material(manager.CardShader);
+				_imageMaterial.mainTexture = manager.CardBack;
+				manager.StartCoroutine(manager.LoadCardImageInto(this, _imageMaterial));
+			}
+			return _imageMaterial;
+		}
+	}
+	
+	public string ImageURL {
+		get {
+			string nameFixed = Name.Replace(" ", "%20");
+			return "http://deckbox.org/mtg/" + nameFixed + "/tooltip.jpg";
+		}
+	}
+	
+	public void AddInfo(CardInfo info) {
+		if (info.Name != _name) throw new UnityException("Trying to add info of two different cards together");
+		
+		_ids.AddRange(info.IDs);
+		_cost = info.Cost;
+		_power = info.Power;
+		_toughness = info.Toughness;
+		_rules = info.Rules;
+		_flavor = info.Flavor;
+		
+		if (Updated != null) Updated();
+	}
+}
+
--- Unity/Assets/Resources/Card/Card.cs
+++ Unity/Assets/Resources/Card/Card.cs
@@ -3,9 +3,30 @@ using System.Collections;
 
 public class Card : MonoBehaviour {
 	
-	public string _name = "Frenetic Raptor";
+	const int MAX_LETTERS_WIDTH_NAME = 14;
+	
+	public string testName;
+	
+	public TextMesh _powerTouchness;
+	
+	private CardInfo _info;
 	
 	void Start () {
-		renderer.material = CardInfoManager.Instance.CardInfo[_name].ImageMaterial;
+		if (!string.IsNullOrEmpty(testName)) SetInfo(testName);
+	}
+	
+	public void SetInfo(string name) {
+		if (_info != null) _info.Updated -= UpdateInfo;
+		_info = CardInfoManager.Instance.GetCardInfo(name);
+		_info.Updated += UpdateInfo;
+		UpdateInfo();
+	}
+	
+	public void UpdateInfo() {
+		renderer.material = _info.ImageMaterial;
+		
+		_powerTouchness.text =
+			_info.Power != CardInfo.UNUSED_VALUE && _info.Toughness != CardInfo.UNUSED_VALUE ?
+			(_info.Power + "/" + _info.Toughness) : " ";
 	}
 }
